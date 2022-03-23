b7d5fe1ec843cd62b1c0aeacb134a3ad59f551af
gaurav <gourav14cs17@gmail.com>
2022-03-24 00:34:15 +0530

    add test_resluts

--- teethu2net_test.py
+++ teethu2net_test.py
@@ -40,7 +40,7 @@ class TestModel:
         self.batch_size_train = config['batch_size']
         self.root_dir = ""
 
-        self.prediction_dir = os.path.join(self.root_dir, "test_data", self.model_name + "Masks_results" + os.sep)
+        self.prediction_dir = os.path.join(self.root_dir, "Results", self.model_name + "Masks_results" + os.sep)
         image_dir = os.path.join(self.root_dir, config['data_folder_name'], "Images")
         self.img_name_list = glob.glob(image_dir + os.sep + "*")
         teeth_dataset = Teeth_dataloader(img_name_list=self.img_name_list, lbl_name_list=[],
